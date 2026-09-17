de9b0fc852d953c7ad53fed41523d64a70bc479c
Salam Sahane <salamsahane@gmail.com>
2026-09-17 02:09:00 +0100

    chore: initialise spring boot for the backend

---
+++ backend/src/main/java/com/salamsahane/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.salamsahane.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+}
